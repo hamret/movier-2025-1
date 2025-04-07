680c71cba449bf4fb49ba5cbb00da147be83d8ad
Daoll <daoll43000@gmail.com>
2025-04-07 15:49:30 +0900

    장르 추천 코드 완성

--- front/movie_js.js
+++ front/movie_js.js
@@ -14,9 +14,10 @@ let MovieObject = {
              console.log(response)
              movielist = response.result
 
-             topdiv = document.createElement("div")
-             topdiv.style = "column-count:5"
-             document.body.appendChild(topdiv)
+            //  topdiv = document.createElement("div")
+            //  topdiv.style = "column-count:5"
+            //  document.body.appendChild(topdiv)
+            topdiv = document.getElementById("alldiv")
 
              movielist.forEach(movie => {
 
@@ -41,8 +42,59 @@ let MovieObject = {
             console.log(error)
 
         });
-    }
-}
+    },
 
+        getgenres: function(){
+
+            Genre = document.getElementById("sgenre").value;
+
+            $.ajax({
+                // 실행할 코드
+                        type: "GET",
+                        url: "http://localhost:8000/genresbest/" + Genre,
+                }).done(function(response){
+                    console.log(response);
+
+                     console.log(response)
+                     movielist = response.result
+
+                     movielist.forEach(movie => {
+                        cmovie = document.createElement("div");
+                        cmovie.className = "card";
+
+                    topdiv = document.getElementById("genrediv")
+        
+                    //1번 방법 topdiv.innerHTML = "";
+                    //2번방법
+                    while(topdiv.firstChild){
+                        topdiv.removeChild(topdiv.firstChild)
+                    }
+
+                     movielist.forEach(movie => {
+        
+                        cmovie = document.createElement("div")
+                        cmovie.className = "card"
+        
+                        mimg = document.createElement("img")
+                        mimg.className = "card-img-top"
+                        mimg.src = movie.poster_path
+                        mimg.style.cursor = "pointer"
+                        mimg.onclick = function(){
+                            window.open(movie.url)
+                        }
+                        cmovie.appendChild(mimg)
+                        topdiv.appendChild(cmovie)
+                     });
+        
+        
+                }).fail(function(error){
+                // 실패코드
+                    console.log(error)
+        
+                });
+
+        }
+    )}
+}
 MovieObject.init();
 MovieObject.getall();
